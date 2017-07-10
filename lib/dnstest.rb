41e1fd392a1c1cc0c51ed1e615bac25e207a9d41
iagox86 <ron-git@skullsecurity.org>
2017-07-09 18:20:35 -0700

    Port dnstest to the new Nesser library

--- lib/dnstest.rb
+++ lib/dnstest.rb
@@ -10,73 +10,111 @@
 
 $LOAD_PATH << File.dirname(__FILE__) # A hack to make this work on 1.8/1.9
 
+require 'nesser'
+require 'socket'
 require 'trollop'
-require '../server/libs/dnser'
 
-# version info
-NAME = "dnstest"
-VERSION = "v1.0.0"
+require 'version'
 
 Thread.abort_on_exception = true
 
-# Options
-opts = Trollop::options do
-  version(NAME + " " + VERSION)
+puts <<EOS
+This script determines if you hold the authoritative record for a domain by
+sending a request for a random subdomain. If the request goes through, you hold
+the record!
+--
+EOS
 
-  opt :version, "Get the #{NAME} version",                             :type => :boolean, :default => false
-  opt :host,    "The ip address to listen on",                         :type => :string,  :default => "0.0.0.0"
-  opt :port,    "The port to listen on",                               :type => :integer, :default => 53
-  opt :domain,  "The domain to check",                                 :type => :string,  :default => nil,      :required => true
-  opt :timeout, "The amount of time (seconds) to wait for a response", :type => :integer, :default => 10
-end
+module DnsUtils
+  MY_NAME = "dnstest (#{NAME}) #{VERSION}"
 
-if(opts[:port] < 0 || opts[:port] > 65535)
-  Trollop::die :port, "must be a valid port (between 0 and 65535)"
-end
+  # Options
+  opts = Trollop::options do
+    version(MY_NAME)
 
-if(opts[:domain].nil?)
-  Trollop::die :domain, "Domain is required!"
-end
-
-puts("Starting #{NAME} #{VERSION} DNS server on #{opts[:host]}:#{opts[:port]}")
-
-domain = (0...16).map { ('a'..'z').to_a[rand(26)] }.join() + "." + opts[:domain]
-
-dnser = DNSer.new(opts[:host], opts[:port])
-
-dnser.on_request() do |transaction|
-  request = transaction.request
-
-  if(request.questions.length < 1)
-    puts("The request didn't ask any questions!")
-    next
+    opt :version, "Get the #{MY_NAME} version (spoiler alert)", :type => :boolean, :default => false
+    opt :host,    "The ip address to listen on", :type => :string,  :default => "0.0.0.0"
+    opt :port,    "The port to listen on", :type => :integer, :default => 53
+    opt :domain,  "The domain to check", :type => :string,  :default => nil,      :required => true
+    opt :timeout, "The amount of time (seconds) to wait for a response", :type => :integer, :default => 10
+    opt :upstream, "The upstream DNS server to send requests to, host:port", :type => :string, :default => "8.8.8.8:53"
   end
 
-  if(request.questions.length > 1)
-    puts("The request asked multiple questions! This is super unusual, if you can reproduce, please report!")
-    next
+  if opts[:port] < 0 || opts[:port] > 65535
+    Trollop::die(:port, "must be a valid port (between 0 and 65535)")
   end
 
-  question = request.questions[0]
-  puts("Received: #{question}")
-  if(question.type == DNSer::Packet::TYPE_A && question.name == domain)
-    puts("You have the authoritative server!")
-    transaction.error!(DNSer::Packet::RCODE_NAME_ERROR)
-    exit()
+  if opts[:domain].nil?
+    Trollop::die :domain, "Domain is required!"
+  end
+
+  upstream_host, upstream_port = opts[:upstream].split(/:/, 2)
+  if upstream_host.nil?
+    upstream_host = '8.8.8.8'
+  end
+  if upstream_port.nil?
+    upstream_port = 53
   else
-    puts("Received a different request: #{question}")
+    upstream_port = upstream_port.to_i
   end
 
-  # Always respond with an error
-  transaction.error!(DNSer::Packet::RCODE_NAME_ERROR)
+  puts("Starting #{MY_NAME} DNS server on #{opts[:host]}:#{opts[:port]}...")
+  puts()
+
+  # Generate a random subdomain
+  domain = (0...16).map { ('a'..'z').to_a[rand(26)] }.join() + "." + opts[:domain]
+
+  s = UDPSocket.new()
+
+  puts("(Listening for #{domain})")
+  Nesser::Nesser.new(s: s, host: opts[:host], port: opts[:port]) do |transaction|
+    request = transaction.request
+
+    if(request.questions.length < 1)
+      puts("The request didn't ask any questions!")
+      next
+    end
+
+    if(request.questions.length > 1)
+      puts("The request asked multiple questions! This is super unusual, if you can reproduce, please report!")
+      next
+    end
+
+    question = request.questions[0]
+    puts("(Received: #{question})")
+
+    # Check if it's the request that we sent
+    if(question.type == Nesser::TYPE_A && question.name == domain)
+      puts("You have the authoritative server!")
+
+      # Just sent back a name error
+      transaction.error!(Nesser::RCODE_NAME_ERROR)
+      exit()
+    else
+      puts("This is not the request we're looking for!")
+      transaction.error!(Nesser::RCODE_NAME_ERROR)
+    end
+  end
+
+  # Perform the request and ignore the result
+  puts("(Requesting #{domain} from #{upstream_host}:#{upstream_port})")
+
+  begin
+    result = Nesser::Nesser.query(
+      s: s,
+      hostname: domain,
+      server: upstream_host,
+      port: upstream_port,
+      type: Nesser::TYPE_A,
+      cls: Nesser::CLS_IN,
+      timeout: opts[:timeout],
+    )
+    puts("Got a response from the DNS server, but didn't see the request... you probably don't have the authoritative server. :(")
+    puts()
+    puts(result)
+  rescue Nesser::DnsException => e
+    puts("Request returned an error... you probably don't have the authoritative server. :(")
+    puts()
+    puts(e)
+  end
 end
-
-puts("Sending: #{domain}!")
-DNSer.query(domain, { :type => DNSer::Packet::TYPE_A }) do |response|
-  # Do nothing
-end
-
-sleep(opts[:timeout])
-
-puts("Request timed out... you probably don't have the authoritative server. :(")
-exit(0)
